fbb57a5c53bc0579b1ee211d9a7a082ecd6ea207
rizaldinur <rnur71@gmail.com>
2024-06-11 20:20:28 +0700

    update post functionality

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let data = new Array();
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", data);
 });
 
+app.post("/submit-blog", (req, res) => {
+  data.push(req.body);
+  console.log(data[0]);
+  res.redirect("/");
+});
 app.listen(port, () => {
   console.log("Server running on port" + port);
 });
