82f3bf80bccc4628b51e922968a532b5cffc0f06
rizaldinur <rnur71@gmail.com>
2024-06-14 11:05:11 +0700

    enable post function

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 import express from "express";
 import bodyParser from "body-parser";
+import jsdom from "jsdom";
 
+const { JSDOM } = jsdom;
 const app = express();
 const port = 3000;
+const { document } = new JSDOM(``, {
+  url: "http://localhost:" + port,
+}).window;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let data = new Array();
+let data = [];
+let btnId = document.querySelector("#btn-del");
+console.log(btnId.innerHTML);
 app.get("/", (req, res) => {
-  res.render("index.ejs", data);
+  // console.log(data);
+
+  if (data.length === 0) {
+    res.render("index.ejs");
+  } else {
+    res.render("index.ejs", { blog: data });
+  }
 });
 
 app.post("/submit-blog", (req, res) => {
   data.push(req.body);
-  console.log(data[0]);
+  console.log(data);
   res.redirect("/");
 });
+
+app.post("/update-blog", (req, res) => {});
+
+app.post("/delete", (req, res) => {});
+
 app.listen(port, () => {
   console.log("Server running on port" + port);
 });
