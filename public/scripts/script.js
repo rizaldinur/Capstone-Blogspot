fdb2c92c512a6b1020407173bdcdfde5ff1cb1d6
rizaldinur <rnur71@gmail.com>
2024-06-21 19:23:35 +0700

    update delete function
    make overlay confirmation box since it is a dangerous action

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,19 @@
 // overlay delete
+$(".btn-del").on("mousedown", function (e) {
+  $("#overlay-del").addClass("show-overlay");
+  $("#btn-delete").val($(this).val());
+});
+
+$(".btn-cancel").on("mousedown", function (e) {
+  $("#overlay-del").removeClass("show-overlay");
+  $("#btn-delete").removeAttr("value");
+});
 // overlay put
 // closeoverlaybtn put
 // close overlay on click outside box
+$(window).on("mousedown", function (e) {
+  if ($(e.target).is(".overlay")) {
+    $(".overlay").removeClass("show-overlay");
+    $("#btn-delete").removeAttr("value");
+  }
+});
